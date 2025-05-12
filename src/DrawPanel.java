d43862feb858c9141199efbae71f48c63c9611d5
hleb <werfdivjfnewiowdsj@gmail.com>
2025-05-12 09:08:13 -0400

    All finished, program should now work as intended

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -17,44 +17,34 @@ public class DrawPanel extends JPanel implements MouseListener {
 
     public DrawPanel() {
         this.addMouseListener(this);
-        this.b = new BrickLayout("src/bricks", 40, true);
+        this.b = new BrickLayout("src/bricks", 40, false);
         brickLayout = b.getBrickLayout();
         this.bricks = b.getBackUp();
+        b.doOneBrick(0);
     }
 
 
     protected void paintComponent(Graphics g) {
 
-        super.paintComponent(g);
+            super.paintComponent(g);
 
-        int x = 10;
-        int y = 10;
+            int x = 10;
+            int y = 10;
 
-        Graphics2D g2 = (Graphics2D)g;
+            Graphics2D g2 = (Graphics2D) g;
 
-        int count = 0;
-        while(count != bricks.size()){
+            if (count < 30) {
+                fallingBricks.add(bricks.get(count));
+            }
 
-
-                if(count != bricks.size()){
-                    fallingBricks.add(bricks.get(count));
-                    Brick current = bricks.get(count);
-
-                    int start = current.getStart();
-                    int end = current.getEnd();
-
-                    for(int i = start; i <= end; i ++ ){
-                        fallLayout[current.getFall()][i] = 1;
+            for (int i = 0; i < fallingBricks.size(); i++) {
+                if (fallingBricks.get(i).getFall() != fallingBricks.get(i).getHeight()) {
+                    for (int j = fallingBricks.get(i).getStart(); j <= fallingBricks.get(i).getEnd(); j++) {
+                        fallLayout[fallingBricks.get(i).getFall() + 1][j] = 1;
+                        fallLayout[fallingBricks.get(i).getFall()][j] = 0;
                     }
+                    fallingBricks.get(i).setFall(fallingBricks.get(i).getFall() + 1);
                 }
-
-                for(int i = 0; i < fallingBricks.size(); i ++){
-                    if(fallingBricks.get(i).getFall() != fallingBricks.get(i).getHeight()){
-                        fallingBricks.get(i).setFall(fallingBricks.get(i).getFall() + 1);
-                    }
-                }
-
-                count ++;
 
                 for (int c = 0; c < 40; c++) {
                     for (int r = 0; r < 30; r++) {
@@ -65,24 +55,35 @@ public class DrawPanel extends JPanel implements MouseListener {
                             g2.fillRect(x, y, 20, 20);
                             g2.setColor(Color.BLACK);
                         }
-
                         y += 25;
                     }
                     x += 25;
                     y = 10;
+
                 }
             }
+            count++;
+            b.doOneBrick(1);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void mouseClicked(MouseEvent e) {
     }
+
     public void mousePressed(MouseEvent e) {
     }
+
     public void mouseReleased(MouseEvent e) {
     }
+
     public void mouseEntered(MouseEvent e) {
     }
+
     public void mouseExited(MouseEvent e) {
     }
 }
